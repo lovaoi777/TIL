a7994159f12b518173d771cb0fb0eac3a99e38f3
lovaoi777 <lovaoi777@naver.com>
2024-02-02 23:24:47 +0900

    Image Setting

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/app/_components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
