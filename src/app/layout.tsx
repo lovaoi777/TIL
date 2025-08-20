67678ca0c23927d265b94f8fa358abc2b9d6621e
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2025-08-20 13:32:59 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <header> 
+      <head> 
         <meta name="naver-site-verification" content="97d9c75aaebfe2e5d844c3ab3c6efb71b5ca9fc4" />
-      </header>
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
