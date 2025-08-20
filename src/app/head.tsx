ed5a765e869f5129be9339100b659eb8dfdf9594
lovaoi777 <77834532+lovaoi777@users.noreply.github.com>
2025-08-20 13:39:23 +0900

    setting : naver search

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="naver-site-verification" content="97d9c75aaebfe2e5d844c3ab3c6efb71b5ca9fc4" />
+      <title>Byeonggyutil</title>
+    </>
+  );
+}
